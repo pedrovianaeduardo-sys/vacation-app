4d2bf96cba7f74e4d7aa4d071ef33953312be5f9
pedrovianaeduardo-sys <pedrovianaeduardo@gmail.com>
2026-02-24 05:43:48 +0000

    modified:   src/supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+
 export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
+  supabaseUrl,
+  supabaseAnonKey
 );
